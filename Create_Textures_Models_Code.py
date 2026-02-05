6b9e9ff6ecde59cbddbb3840212361f7f0db40af
sagieppel <sagieppel@gmail.com>
2026-02-05 22:33:30 +0200

    Update Create_Textures_Models_Code.py

--- Create_Textures_Models_Code.py
+++ Create_Textures_Models_Code.py
@@ -277,15 +277,12 @@ if __name__=="__main__":
         recheck_originality=False #True # double check the idea is in dataset also identify related ideas
         model = "google/gemini-3-flash-preview" #"z-ai/glm-4.7-flash"#"moonshotai/kimi-k2.5"#openai/gpt-5.2"#grok-4-fast-reasoning"#"Qwen/Qwen2.5-VL-72B-Instruct"#"claude-sonnet-4-5-20250929" #"grok-4-fast-reasoning"#"gemini-2.5-flash"#"Qwen/Qwen2.5-VL-72B-Instruct"#"gpt-5"
         for kk in range(1000):
-            # if kk==0:
-            #     number_of_new= 0 # allow the method to finish existing un implemented ideas
-            # else:
-            #     number_of_new= 4 # Number of new ideas to suggest in each round (
-            number_of_new = 5
-            if kk==0: number_of_new = 0
+            number_of_new= 4 # Number of new ideas to suggest in each round (
+ 
+        #    if kk==0: number_of_new = 0
 
 
-            # Number of new ideas to suggest in each round (
+             
             #try:
             generate_scitextures(dataset_dir=outputdir, query_dir=query_dir, number_of_new=number_of_new, number_of_code_fix_retry=number_of_code_fix_retry, recheck_originality=recheck_originality, model=model)
             # except Exception as e:
